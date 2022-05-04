2fa65e4b4d3c8c7aa3107b23ffc5829b2c850de2
palerme <joaoguilhermefagundes@gmail.com>
2022-05-04 02:18:37 -0300

    1:14:13 - Aula parada!

--- src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
+++ src/main/java/com/dio/santander/bankline/api/model/Movimentacao.java
@@ -26,6 +26,9 @@ public class Movimentacao {
 	
 	@Enumerated(EnumType.STRING)
 	private MovimentacaoTipo tipo;
+	
+	@Column (name = "id_conta")
+	private Integer idConta;
 	public Integer getId() {
 		return id;
 	}
@@ -56,5 +59,10 @@ public class Movimentacao {
 	public void setTipo(MovimentacaoTipo tipo) {
 		this.tipo = tipo;
 	}
-	
+	public Integer getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(Integer idConta) {
+		this.idConta = idConta;
+	}
 }
